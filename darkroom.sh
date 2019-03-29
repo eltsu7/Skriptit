cae8bf4613fa7ea8fb25c88df9e98a67a09ad29d
Eeli Hernesniemi <eeli.hernesniemi@gmail.com>
2019-03-29 19:56:18 +0200

    timeout curlille, toimii paremmin huonossa netissä

--- darkroom.sh
+++ darkroom.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo $(curl -s https://ttkamerat.fi/darkroom/api/v1/sensors/latest) | jq -r 'if .entries[0].value > 100 then "Joku on pimiöllä :O" else "Pimiö tyhjä :(" end'
+echo $(curl -s https://ttkamerat.fi/darkroom/api/v1/sensors/latest --connect-timeout 2) | jq -r 'if .entries[0].value > 100 then "Joku on pimiöllä :O" else "Pimiö tyhjä :(" end'
 
